84d67c6b54307253581af606ebac830fdbdcdd2a
Prakash9019 <plsprakash2003@gmail.com>
2025-06-01 20:08:17 +0530

    added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const foodRoutes = require('./routes/food');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 const authRoutes = require('./routes/authRoutes');
 
